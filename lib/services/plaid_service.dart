05cacc5a8f695ae67fce04cd15bc736ae68e444f
Nick Venhuizen <nvenhuizen14@nvsolutions.online>
2023-12-29 03:10:57 -0500

    http change

--- lib/services/plaid_service.dart
+++ lib/services/plaid_service.dart
@@ -36,7 +36,7 @@ class PlaidService {
 
       // Include the client_user_id in the body of the request
       var response = await http.post(
-        Uri.parse('https://n8n.nvsolutionss.com/fetchLinkToken'),
+        Uri.parse('https://n8n.nvsolutionss.com/webhook/fetchLinkToken'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode({
           'client_user_id': userId, // Include the client_user_id
@@ -94,7 +94,7 @@ class PlaidService {
       }
 
       var response = await http.post(
-        Uri.parse('https://n8n.nvsolutionss.com/publicToken'),
+        Uri.parse('https://n8n.nvsolutionss.com/webhook/publicTokenExchange'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode({
           'public_token': publicToken,
